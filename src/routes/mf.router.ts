9210a17b87c1a4a6e66830a053f103e3d3388378
pallavi-patel-developer <pallavi-patel-developer@panthar-infohub.com>
2026-08-20 20:19:55 +0530

    feat(OPS-1): consolidate MF transaction routes

---
+++ src/routes/mf.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { mf_purchase_router } from "./mf-purchase.router.js";
+import { mf_purchase_plan_router } from "./mf-purchase-plan.router.js";
+import { mf_redemption_plan_router } from "./mf-redemption-plan.router.js";
+
+export const mf_router = Router();
+
+mf_router.use("/purchase", mf_purchase_router);
+mf_router.use("/purchase-plan", mf_purchase_plan_router);
+mf_router.use("/redemption-plan", mf_redemption_plan_router);
